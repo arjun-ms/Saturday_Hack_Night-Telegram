cc72566a342d55e2cd0b94a5937c954b94821d30
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2021-09-18 20:42:24 +0530

    Star

--- Code_Buddies/main.py
+++ Code_Buddies/main.py
@@ -1,1 +1,15 @@
-# lorem ipsum   2
+import os
+import telebot
+
+API_KEY = os.getenv('API_KEY')
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['greet'])
+def greet(message):
+  bot.reply_to(message, "Hey! Hows it going?")
+
+@bot.message_handler(commands=['hello'])
+def hello(message):
+  bot.send_message(message.chat.id, "Hello!")
+
+bot.polling()
