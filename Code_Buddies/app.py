fde435393509cf98dc4dc79fb0e08ba505b81243
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2021-09-19 10:29:48 +0530

    Message Structure

--- Code_Buddies/app.py
+++ Code_Buddies/app.py
@@ -37,91 +37,108 @@ def strtMsgEdt(call):
 		
 		try:
 			aboutDev = f'''About:
-
-Lang Used: Python🐍
-[Source Code](https://github.com/arjun-ms/Saturday_Hack_Night-Telegram/tree/main/Code_Buddies)
-
-if you ❤ this, Star this repo
-
-'''
+			Lang Used: Python🐍
+			[Source Code](https://github.com/arjun-ms/Saturday_Hack_Night-Telegram/tree/main/Code_Buddies)
+			if you ❤ this, Star this repo
+			'''
 			key = types.InlineKeyboardMarkup()
 			key.add(types.InlineKeyboardButton("🔙 Home 🏡", callback_data="back"))
 			bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text = aboutDev, disable_web_page_preview=True, reply_markup=key)
-
+		
 		except:
 			pass
+		
+	elif edit == 'imgsToPdfEdit':
+		
+		try:
+			expMsg = f'''
+		Images to pdf :
+		Just Send/forward me some images. When you are finished; use /generate to get your pdf..😉
 
-# @bot.callback_query_handler(func=lambda call: call.data)
-# def strtMsgEdt(call):
-#     edit = call.data
-
-#     if edit == 'strtDevEdt':
-
-#         try:
-#             aboutDev = f'''
-
-# Lang Used: Python🐍
-# [Source Code](https://github.com/arjun-ms/Saturday_Hack_Night-Telegram/tree/main/Code_Buddies)
-
-# if you ❤ this, Star this repo
-
-# '''
-#             key = types.InlineKeyboardMarkup()
-#             key.add(types.InlineKeyboardButton("Home 🏡", callback_data="back"))
-#             bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-#                                   disable_web_page_preview=True, reply_markup=key)
-
-        # except:
-        #     pass
-
-    elif edit == 'imgsToPdfEdit':
-
-        try:
-            expMsg = f'''
-Images to pdf :
-
-		Just Send/forward me some images. When you are finished; use /generate to get your pdf..
-
- ◍ Image Sequence will be considered 
- ◍ For better quality pdfs(send images without Compression) 
+		 ◍ Image Sequence will be considered 🤓
+		 ◍ For better quality pdfs(send images without Compression) 🤧
  
- ◍ `/cancel` - Delete's the current Queue 
- ◍ `/id` - to get your telegram ID 
- '''
-            key = types.InlineKeyboardMarkup()
-            key.add(types.InlineKeyboardButton("Home 🏡", callback_data="back"),
-                    types.InlineKeyboardButton("WARNING ⚠️", callback_data="warning"))
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                  text=expMsg, disable_web_page_preview=True, reply_markup=key)
+		 ◍ `/cancel` - Delete's the current Queue 😒
+		 ◍ `/id` - to get your telegram ID 🤫
+ 
+		 ◍ RENAME YOUR PDF:
+			- By default, your telegram ID will be treated as your pdf name..🙂
+			- `/generate fileName` - to change pdf name to fileName🤞
+			- `/generate name` - to get pdf with your telegram name
+		'''
+			key = types.InlineKeyboardMarkup()
+			key.add(types.InlineKeyboardButton("🔙 Home 🏡", callback_data="back"),types.InlineKeyboardButton("PDF to images ➡️", callback_data="pdfToImgsEdit"))
+			bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text = expMsg, disable_web_page_preview=True, reply_markup=key)
+		
+		except:
+			pass
+		
+	elif edit == 'pdfToImgsEdit':
+		
+		try:
+			expMsg = f'''
+        PDF to images:
 
-        except:
-            pass
+		Just Send/forward me a pdf file.
 
-    elif edit == 'warning':
+ 		◍ I will Convert it to images ✌️
+ 		◍ if Multiple pages in pdf(send as albums) 😌
+ 		◍ Page numbers are sequentially ordered 😬
 
-        try:
-            expMsg = f'''
-	WARNING MESSAGE ⚠️:
+		⚠️ Due to overload this bot will only convert files less than 10mb files..⚠️
 
-	◍ This bot is completely free to use so please dont spam here 
+		if you need to convert 10mb+ you can create your own bot.. Source code is mentioned in bio 😇
+		'''
+			key = types.InlineKeyboardMarkup()
+			key.add(types.InlineKeyboardButton("🔙 Imgs To Pdf", callback_data="imgsToPdfEdit"),types.InlineKeyboardButton("Home 🏡", callback_data="back"),types.InlineKeyboardButton("Warning ⚠", callback_data="warningEdit"))
+			bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text = expMsg, disable_web_page_preview=True, reply_markup=key)
+		
+		except:
+			pass
+		
+	
+	elif edit == 'warningEdit':
+		
+		try:
+			expMsg = f'''
+			WARNING MESSAGE ⚠️:
 
-	◍ Please don't try to spread 18+ contents 
-	'''
-            key = type.InlineKeyboardMarkup()
-            key.add(type.InlineKeyboardButton(
-                type.InlineKeyboardButton("Home 🏡", callback_data="back")))
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                  text=expMsg, disable_web_page_preview=True, reply_markup=key)
+			◍ This bot is completely free to use so please dont spam here 🙏
 
-        except:
-            pass
+			◍ Please don't try to spread 18+ contents 😒
 
-    elif edit == 'close':
-        try:
-            bot.delete_message(chat_id=call.message.chat.id,
-                               message_id=call.message.message_id)
-        except:
-            pass
+			'''
+			key = types.InlineKeyboardMarkup()
+			key.add(types.InlineKeyboardButton("🔙 Back", callback_data="pdfToImgsEdit"),types.InlineKeyboardButton("Home 🏡", callback_data="back"))
+			bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text = expMsg, disable_web_page_preview=True, reply_markup=key)
+		
+		except:
+			pass
+		
+	elif edit == 'back':
+		
+		try:
+			strtMsg = f'''
+		Hey..!! This bot will helps you to do many things with pdf's 🥳
+
+		Some of the main features are:
+		◍ `Convert images to PDF`
+		◍ `Convert PDF to images`
+		'''
+			key = types.InlineKeyboardMarkup()
+			key.add(types.InlineKeyboardButton("Source Code ❤️", callback_data="strtDevEdt"),types.InlineKeyboardButton("Explore More 🥳", callback_data="imgsToPdfEdit"))
+			bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text = strtMsg, disable_web_page_preview=True, reply_markup=key)
+		
+		except:
+			pass
+		
+	elif edit == 'close':
+		
+		try:
+			bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+			
+		except:
+			pass
 
 
 @bot.message_handler(commands=["id"])
