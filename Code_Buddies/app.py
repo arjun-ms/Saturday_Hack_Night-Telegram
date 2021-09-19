768df90663f8b5c7704d8046e474a63a93017aa4
Arjun M S <arjunachu123.aa@gmail.com>
2021-09-19 08:01:13 +0530

    Start button added

--- Code_Buddies/app.py
+++ Code_Buddies/app.py
@@ -1,17 +1,29 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
 import telebot
-from telebot import types,util
+from telebot import types
+from telebot.types import InputMediaPhoto
+from PIL import Image
+import shutil
+from time import sleep
+import fitz
 
-bot = telebot.TeleBot(os.environ.get("API_KEY"))
-@bot.message_handler(commands=['start', 'help'])
+API_TOKEN = os.getenv("API_TOKEN")
+bot = telebot.TeleBot(API_TOKEN, parse_mode="Markdown")
+
+@bot.message_handler(commands=["start"])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	try:
+		bot.send_chat_action(message.chat.id, "typing")
+		strtMsg = f'''
+Hey [{message.from_user.first_name}](tg://user?id={message.chat.id})..!! This bot will helps you to do many things with pdf's 🥳
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	if(message.text.lower() in ["hi","hello","start"]):
-		bot.reply_to(message, "Hello 👋")
+Some of the main features are:
+◍ `Convert images to PDF``
+'''
+		key = types.InlineKeyboardMarkup()
+		key.add(types.InlineKeyboardButton("Source Code ❤️", callback_data="strtDevEdt"),types.InlineKeyboardButton("Explore More 🥳", callback_data="imgsToPdfEdit"))
+		bot.send_message(message.chat.id, strtMsg, disable_web_page_preview=True, reply_markup=key)
+	
+	except:
+		pass
 
-bot.polling()
